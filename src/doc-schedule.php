e6fa96a8a9cf23b553f7846bb3202c3830ea4c77
rusirukashmeera <it23836204@my.sliit.lk>
2024-10-07 15:55:36 +0530

    doc schedule changes

--- src/doc-schedule.php
+++ src/doc-schedule.php
@@ -4,7 +4,6 @@ session_start();
 
 $firstName = $_SESSION["firstName"];
 $lastName = $_SESSION["lastName"];
-<<<<<<< HEAD
 $email = $_SESSION["email"];
 
 $sql2 = "SELECT Doctor_Id FROM doctor WHERE Email = '$email'";
@@ -14,14 +13,6 @@ $result2 = mysqli_query($conn, $sql2);
 if (mysqli_num_rows($result2) > 0) {
     $row2 = mysqli_fetch_assoc($result2);
     $docID = $row2["Doctor_Id"];
-=======
-
-if(isset($_POST["logout"])){
-    session_unset();
-    session_destroy();
-    mysqli_close($conn);
-    header("Location: index.php");
->>>>>>> 6db4a15f7e4e27ee44bdcda23323812cb97cc6fd
 }
 ?>
 
